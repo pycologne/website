fd3c2eb1d7f70caf1a83432523d663368e6941aa
Christopher Arndt <chris@chrisarndt.de>
2014-01-26 19:32:05 +0100

    Further adaption of fab commands to server installation

--- fabfile/remote.py
+++ fabfile/remote.py
@@ -2,30 +2,32 @@
 """Fab commands that are running on remotes."""
 
 from __future__ import with_statement
+
+import os
+from os.path import expanduser, isfile, join
+
 from fabric.api import *
 from fabric.context_managers import shell_env
 
-from os.path import expanduser, isfile, join
+USER = 'pycologne'
+HOME = '/home/www/pycologne.de'
+ENV = os.getenv('ENV', 'staging')
+SITE_NAME = ENV
+PROJECT_NAME = 'pycologne'
+PROJECT_DIR = join(HOME, 'sites', SITE_NAME)
+VIRTUAL_ENV_NAME = '%s-%s' % (PROJECT_NAME, SITE_NAME)
 
-env.hosts = ['pycologne.de']
+env.hosts = ['%s@pycologne.de' % USER]
 
 if env.ssh_config_path and isfile(expanduser(env.ssh_config_path)):
     env.use_ssh_config = True
-
-APP_USER = 'pycologne'
-APP_HOME = '/home/www/pycologne.de'
-PROJECT_NAME = 'pycologne'
-ENV = 'staging'
-
-PROJECT_DIR = join(APP_HOME, 'sites', PROJECT_NAME)
-VIRTUAL_ENV_NAME = '%s-%s' % (PROJECT_NAME, ENV)
 
 
 def supervisord(task='start'):
     """Controls the supervisord."""
     if task == 'start':
         with prefix('workon %s' % VIRTUAL_ENV_NAME):
-            run('supervisord -c "%s/etc/supervisord.conf"' % APP_HOME)
+            run('supervisord -c "%s/etc/supervisord.conf"' % HOME)
     elif task == 'stop':
         supervisorctl('shutdown')
     elif task == 'restart':
@@ -36,15 +38,16 @@ def supervisord(task='start'):
 def supervisorctl(task):
     """Controls the supervisor."""
     with prefix('workon %s' % VIRTUAL_ENV_NAME):
-        run('supervisorctl -c "%s/etc/supervisor.conf" %s %s' % (APP_HOME, task, PROJECT_NAME))
+        run('supervisorctl -c "%s/etc/supervisor.conf" %s %s' %
+            (HOME, task, SITE_NAME))
 
 
 def memcached(task='start'):
     """Controls the memcached."""
     if task == 'start':
-        run('memcached -d -m 256 -P $HOME/var/run/memcached.pid')
+        run('memcached -d -m 256 -P %s/var/run/memcached.pid' % HOME)
     elif task == 'stop':
-        run('kill $(cat $HOME/var/run/memcached.pid)')
+        run('kill $(cat %s/var/run/memcached.pid)' % HOME)
     elif task == 'restart':
         memcached('stop')
         memcached('start')
@@ -60,7 +63,7 @@ def update_source():
     with cd(PROJECT_DIR), prefix('workon %s' % VIRTUAL_ENV_NAME):
         run('git pull')
         run('pip install --no-deps -Ur requirements/stable.req.txt')
-        with shell_env(ENV='production'):
+        with shell_env(ENV=ENV):
             run('./manage.py syncdb --migrate')
             run('./manage.py collectstatic --noinput')
 
@@ -68,7 +71,7 @@ def update_source():
 def clear_remote_static_cache():
     """Clears the django compressor cache on the remote server."""
     with cd(PROJECT_DIR), prefix('workon %s' % VIRTUAL_ENV_NAME):
-        with shell_env(ENV='production'):
+        with shell_env(ENV=ENV):
             run('fab clear_static_cache')
 
 
