c321abbd5866d379c1cec7c794e7dd49aa01f0af
Thomas Koch <tommykoch@gmail.com>
2014-01-26 19:23:08 +0100

    added an CMS App hook for the events

---
+++ pycologne/events/cms_app.py
@@ -0,0 +1,13 @@
+from cms.app_base import CMSApp
+from cms.apphook_pool import apphook_pool
+from django.utils.translation import ugettext_lazy as _
+
+from .menu import EventsMenu
+
+class EventsApp(CMSApp):
+    name = _("events-app")
+    urls = ["pycologne.events.urls"]
+    menus = [EventsMenu] # attach a CMSAttachMenu to this apphook.
+
+# register CMS App
+apphook_pool.register(EventsApp)  
